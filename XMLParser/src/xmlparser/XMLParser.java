8f9151aa300315d58e5eb0f013c974ccd594d3a3
Lucas Hauswald <lucas@the-soulmen.com>
2014-06-20 10:00:12 +0200

    XMLParser can now create one big ass XML file

--- XMLParser/src/xmlparser/XMLParser.java
+++ XMLParser/src/xmlparser/XMLParser.java
@@ -21,52 +21,150 @@ public class XMLParser {
     public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader("CRNH0203-2013-CO_Dinosaur_2_E.txt"));
+
             String line, wbanno, longitude, latitude;
             int numberOfUpdates = 0;
-            String[] updateData = new String[33];
+            String[] updateData = new String[29];
+
+            File xml = new File("CRNH0203-2013-CO_Dinosaur_2_E.xml");
+            FileWriter writer = new FileWriter(xml);
 
             while ((line = reader.readLine()) != null) {
                 if (numberOfUpdates < 1) {
                     wbanno = line.substring(0, 5);
                     longitude = line.substring(41, 48);
                     latitude = line.substring(49, 56);
+
+                    writer.write(
+                            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                            + "<station>\n"
+                            + "\t<wbanno>\n"
+                            + "\t\t" + wbanno + "\n"
+                            + "\t</wbanno>\n"
+                            + "\t<longitude>\n"
+                            + "\t\t" + longitude + "\n"
+                            + "\t</longitude>\n"
+                            + "\t<latitude>\n"
+                            + "\t\t" + latitude + "\n"
+                            + "\t</latitude>\n");
+
+                    writer.flush();
                 }
-                updateData[0] = line.substring(0, 5);       // WBANNO
-                updateData[1] = line.substring(6, 14);      // UTC_Date
-                updateData[2] = line.substring(15, 19);     // UTC_Time
-                updateData[3] = line.substring(34, 40);     // CRX_VN
-                updateData[4] = line.substring(41, 48);     // LONGITUDE
-                updateData[5] = line.substring(49, 56);     // LATITUDE
-                updateData[6] = line.substring(57, 64);     // T_CALC
-                updateData[7] = line.substring(65, 72);     // T_HR_AVG
-                updateData[8] = line.substring(73, 80);     // T_MAX
-                updateData[9] = line.substring(81, 88);     // T_MIN
-                updateData[10] = line.substring(89, 96);    // P_CALC
-                updateData[11] = line.substring(97, 103);   // SOLARAD
-                updateData[12] = line.substring(104, 105);  // SOLARAD_FLAG
-                updateData[13] = line.substring(106, 112);  // SOLARAD_MAX
-                updateData[14] = line.substring(113, 114);  // SOLARAD_MAX_FLAG
-                updateData[15] = line.substring(115, 121);  // SOLARAD_MIN
-                updateData[16] = line.substring(122, 123);  // SOLARAD_MIN_FLAG
-                updateData[17] = line.substring(124, 125);  // SUR_TEMP_TYPE
-                updateData[18] = line.substring(126, 133);  // SUR_TEMP
-                updateData[19] = line.substring(134, 135);  // SUR_TEMP_FLAG
-                updateData[20] = line.substring(136, 143);  // SUR_TEMP_MAX
-                updateData[21] = line.substring(144, 145);  // SUR_TEMP_MAX_FLAG
-                updateData[22] = line.substring(146, 153);  // SUR_TEMP_MIN
-                updateData[23] = line.substring(154, 155);  // SUR_TEMP_MIN_FLAG
-                updateData[24] = line.substring(156, 161);  // RH_HR_AVG
-                updateData[25] = line.substring(162, 163);  // RH_HR_AVG_FLAG
-                updateData[26] = line.substring(188, 195);  // SOIL_MOISTURE_50
-                updateData[27] = line.substring(196, 203);  // SOIL_MOISTURE_100
-                updateData[28] = line.substring(204, 211);  // SOIL_TEMP_5
-                updateData[29] = line.substring(212, 219);  // SOIL_TEMP_10
-                updateData[30] = line.substring(220, 227);  // SOIL_TEMP_20
-                updateData[31] = line.substring(228, 235);  // SOIL_TEMP_50
-                updateData[32] = line.substring(236, 243);  // SOIL_TEMP_100
+
+                // Read one line
+                updateData[0] = line.substring(6, 14).trim();      // UTC_Date
+                updateData[1] = line.substring(15, 19).trim();     // UTC_Time
+                updateData[2] = line.substring(34, 40).trim();     // CRX_VN
+                updateData[3] = line.substring(57, 64).trim();     // T_CALC
+                updateData[4] = line.substring(65, 72).trim();     // T_HR_AVG
+                updateData[5] = line.substring(73, 80).trim();     // T_MAX
+                updateData[6] = line.substring(81, 88).trim();     // T_MIN
+                updateData[7] = line.substring(97, 103).trim();    // SOLARAD
+                updateData[8] = line.substring(104, 105).trim();   // SOLARAD_FLAG
+                updateData[9] = line.substring(106, 112).trim();  // SOLARAD_MAX
+                updateData[10] = line.substring(113, 114).trim();  // SOLARAD_MAX_FLAG
+                updateData[11] = line.substring(115, 121).trim();  // SOLARAD_MIN
+                updateData[12] = line.substring(122, 123).trim();  // SOLARAD_MIN_FLAG
+                updateData[13] = line.substring(124, 125).trim();  // SUR_TEMP_TYPE
+                updateData[14] = line.substring(126, 133).trim();  // SUR_TEMP
+                updateData[15] = line.substring(134, 135).trim();  // SUR_TEMP_FLAG
+                updateData[16] = line.substring(136, 143).trim();  // SUR_TEMP_MAX
+                updateData[17] = line.substring(144, 145).trim();  // SUR_TEMP_MAX_FLAG
+                updateData[18] = line.substring(146, 153).trim();  // SUR_TEMP_MIN
+                updateData[19] = line.substring(154, 155).trim();  // SUR_TEMP_MIN_FLAG
+                updateData[20] = line.substring(156, 161).trim();  // RH_HR_AVG
+                updateData[21] = line.substring(162, 163).trim();  // RH_HR_AVG_FLAG
+                updateData[22] = line.substring(188, 195).trim();  // SOIL_MOISTURE_50
+                updateData[23] = line.substring(196, 203).trim();  // SOIL_MOISTURE_100
+                updateData[24] = line.substring(204, 211).trim();  // SOIL_TEMP_5
+                updateData[25] = line.substring(212, 219).trim();  // SOIL_TEMP_10
+                updateData[26] = line.substring(220, 227).trim();  // SOIL_TEMP_20
+                updateData[27] = line.substring(228, 235).trim();  // SOIL_TEMP_50
+                updateData[28] = line.substring(236, 243).trim();  // SOIL_TEMP_100
+
+                // Write XML update
+                writer.write("\t<update>\n"
+                             + "\t\t<utc_date>\n"
+                             + "\t\t\t" + updateData[0] + "\n"
+                             + "\t\t</utc_date>\n"
+                             + "\t\t<utc_time>\n"
+                             + "\t\t\t" + updateData[1] + "\n"
+                             + "\t\t</utc_time>\n"
+                             + "\t\t<crx_vn>\n"
+                             + "\t\t\t" + updateData[2] + "\n"
+                             + "\t\t</crx_vn>\n"
+                             + "\t\t<temp>\n"
+                             + "\t\t\t<t_calc>\n"
+                             + "\t\t\t\t" + updateData[3] + "\n"
+                             + "\t\t\t</t_calc>\n"
+                             + "\t\t\t<t_hr_avg>\n"
+                             + "\t\t\t\t" + updateData[4] + "\n"
+                             + "\t\t\t</t_hr_avg>\n"
+                             + "\t\t\t<t_max>\n"
+                             + "\t\t\t\t" + updateData[5] + "\n"
+                             + "\t\t\t</t_max>\n"
+                             + "\t\t\t<t_min>\n"
+                             + "\t\t\t\t" + updateData[6] + "\n"
+                             + "\t\t\t</t_min>\n"
+                             + "\t\t</temp>\n"
+                             + "\t\t<solar>\n"
+                             + "\t\t\t<solarad flag=\"" + updateData[8] + "\">\n"
+                             + "\t\t\t\t" + updateData[7] + "\n"
+                             + "\t\t\t</solarad>\n"
+                             + "\t\t\t<solarad_max flag=\"" + updateData[10] + "\">\n"
+                             + "\t\t\t\t" + updateData[9] + "\n"
+                             + "\t\t\t</solarad_max>\n"
+                             + "\t\t\t<solarad_min flag=\"" + updateData[12] + "\">\n"
+                             + "\t\t\t\t" + updateData[11] + "\n"
+                             + "\t\t\t</solarad_min>\n"
+                             + "\t\t</solar>\n"
+                             + "\t\t<sur type=\"" + updateData[13] + "\">\n"
+                             + "\t\t\t<sur_temp flag=\"" + updateData[15] + "\">\n"
+                             + "\t\t\t\t" + updateData[14] + "\n"
+                             + "\t\t\t</sur_temp>\n"
+                             + "\t\t\t<sur_temp_max flag=\"" + updateData[17] + "\">\n"
+                             + "\t\t\t\t" + updateData[16] + "\n"
+                             + "\t\t\t</sur_temp_max>\n"
+                             + "\t\t\t<sur_temp_min flag=\"" + updateData[19] + "\">\n"
+                             + "\t\t\t\t" + updateData[18] + "\n"
+                             + "\t\t\t</sur_temp_min>\n"
+                             + "\t\t</sur_temp>\n"
+                             + "\t\t<rh_hr_avg flag=\"" + updateData[21] + "\">\n"
+                             + "\t\t\t" + updateData[20] + "\n"
+                             + "\t\t</rh_hr_avg>\n"
+                             + "\t\t<soil>\n"
+                             + "\t\t\t<moisture_50>\n"
+                             + "\t\t\t\t" + updateData[22] + "\n"
+                             + "\t\t\t</moisture_50>\n"
+                             + "\t\t\t<moisture_100>\n"
+                             + "\t\t\t\t" + updateData[23] + "\n"
+                             + "\t\t\t</moisture_100>\n"
+                             + "\t\t\t<temp_5>\n"
+                             + "\t\t\t\t" + updateData[24] + "\n"
+                             + "\t\t\t</temp_5>\n"
+                             + "\t\t\t<temp_10>\n"
+                             + "\t\t\t\t" + updateData[25] + "\n"
+                             + "\t\t\t</temp_10>\n"
+                             + "\t\t\t<temp_20>\n"
+                             + "\t\t\t\t" + updateData[26] + "\n"
+                             + "\t\t\t</temp_20>\n"
+                             + "\t\t\t<temp_50>\n"
+                             + "\t\t\t\t" + updateData[27] + "\n"
+                             + "\t\t\t</temp_50>\n"
+                             + "\t\t\t<temp_100>\n"
+                             + "\t\t\t\t" + updateData[28] + "\n"
+                             + "\t\t\t</temp_100>\n"
+                             + "\t\t</soil>\n"
+                             + "\t</update>\n");
+                writer.flush();
 
                 numberOfUpdates++;
             }
+
+            writer.write("</station>\n");
+            writer.flush();
+
+            reader.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(XMLParser.class.getName()).log(Level.SEVERE, null, ex);
             System.err.println("File not found.");
